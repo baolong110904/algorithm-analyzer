a595b27129cbbaaebf4d1da0219bca757cd1d325
baolong110904 <106044858+baolong110904@users.noreply.github.com>
2023-07-16 22:05:57 +0700

    Update insertionsort.cpp

--- algorithms/insertionsort.cpp
+++ algorithms/insertionsort.cpp
@@ -1,17 +1,18 @@
 // O(n^2)
-void InsertionSort_operations(int a[], int n, long long& operations) {
+void InsertionSort_comparison(int a[], int n, int &count_compare) {
+    count_compare = 0;
+
     for (int i = 1; i < n; i++) {
         int key = a[i];
         int j = i - 1;
-        while (j >= 0 && a[j] > key) {
+        while (++count_compare && j >= 0 && a[j] > key) {
             a[j + 1] = a[j];
             j--;
-            operations += 2;  // Count the assignment and comparison operations
         }
         a[j + 1] = key;
-        operations++;  // Count the assignment operation
     }
 }
+
 void InsertionSort_timetaken(int a[], int n) {
     clock_t startTime = clock();  // Start the timer
 
